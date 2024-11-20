3ef27745b9fbae36b275b995288bb16126f4ff2a
longnguyencoder <long0398481719@gmail.com>
2024-11-20 17:36:12 +0700

    update font-end

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-
-const ProductCard = () => {
+import "./ProduitCard.css";
+const ProductCard = ({ product }) => {
   return (
-    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
-      <div>
-        <img className="h-full w-full object-cover object-left-top"
-          src="https://adobus.com.vn/wp-content/uploads/CHIP-KHOAN-TOC-DO-CAO-DFT-HP-CAU-TAO-247x296-1.jpg.webp"
+    <div className="productCard w-[15 rem] m-3 transition-all cursor-pointer">
+      <div className="h-[20rem]">
+        <img
+          className="h-full w-full object-cover object-left-top"
+          src={product.imageUrl}
           alt=""
         />
       </div>
       <div className="textPart bg-white p-3">
         <div>
-            <p>CHIP KHOAN TỐC ĐỘ CAO DFT – HP</p>
-            <a className="font-bold text-red-500 opacity-60" href="https://adobus.com.vn/san-pham/mui-khoan-toc-do-cao-dft-hp">CHIP KHOAN TỐC ĐỘ CAO DFT – HP SERIES φ 5 – 17.60 mm</a>
+          <p>{product.title}</p>
+          <a
+            className="font-bold text-red-500 opacity-60"
+            href="https://adobus.com.vn/san-pham/mui-khoan-toc-do-cao-dft-hp"
+          >
+            {product.brand}
+          </a>
         </div>
         <div className="flex items-center space-x-2">
-                <p className="font-semibold">$199</p>
-                <p className="line-through opacity-50">$1999</p>
-                <p className="text-green-600 font-semibold ">70%off</p>
+          <p className="font-semibold">${product.discountedPrice}</p>
+          <p className="line-through opacity-50">{product.price}</p>
+          <p className="text-green-600 font-semibold ">{product.discountPersent}%off</p>
         </div>
       </div>
     </div>
